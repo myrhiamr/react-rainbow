c4f8191fe6a0a25ec1fd9a2c0d8c5945274a86c6
Myrhiam Rodriguez <myrhiam74@gmail.com>
2024-04-15 18:54:12 -0700

    completed hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import ColorBlock from "./Components/ColorBlock";
+import ColorForm from "./Components/ColorForm";
+
 
 function App() {
+  const [colors, setColors] = useState([]);
+
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+
+  const colorMap = colors.map((color, i) => {
+    return <ColorBlock key={i} color={color} />;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App centered-content">
+    <h1>add your colors!</h1>
+    {colorMap}
+  <ColorForm addColor={addColor}/>
+  </div>
+  )
 }
 
 export default App;
